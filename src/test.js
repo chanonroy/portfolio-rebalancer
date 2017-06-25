14633e9d78fb4bad90748ca0f19bc25afd43ccad
Graeme Westwood <gwestwoo17@gmail.com>
2017-06-25 17:24:42 -0400

    added market value of portfolio

--- src/test.js
+++ src/test.js
@@ -5,24 +5,34 @@ function Balancer() {
      {
        ticker: 'XIC',
        price: 10,
-       quantity: 2,
-       target: 0.5
+       quantity: 5,
+       target: 0.4
      },
      {
        ticker: 'VTI',
        price: 5,
-       quantity: 2,
-       target: 0.5
+       quantity: 3,
+       target: 0.6
      }
    ];
+
+   //Calculate the market value of the portfolio
    this.market_val = this.stocks.reduce(function(acc,val){
      prod = val.price * val.quantity
      return acc + prod
    },0)
 
+   //Cash
    this.cash = 0
+   //Total Capital
    this.capital = this.market_val + this.cash;
    this.actions = [];
+
+   //Check to make sure target weights sum to <= 1
+   this.target_check = this.stocks.reduce(function(acc,val){
+     check_i = val.target
+     return acc + check_i
+   },0)
 
    // ---- Methods ----
 
@@ -34,7 +44,7 @@ function Balancer() {
    this.optimal_calc = function(price, total_money, target) {
 
      var tmp = target * total_money / price;
-     var optimal = Number(tmp);
+     var optimal = Math.round(tmp);
      return optimal;
 
    };
@@ -50,22 +60,83 @@ function Balancer() {
 
    };
 
+   function sortByKey(array, key) {
+
+     return array.sort(function(a,b) { return a[key] - b[key];});
+
+   };
+
    this.main = function() {
 
      for (var i in this.stocks) {
-       var optimal = this.optimal_calc(this.stocks[i].price, this.capital, this.stocks[i].target);
-       var delta = this.rebalance(optimal, this.stocks[i].quantity);
 
-       // conditional for extra capital
+       if (this.target_check > 1) { break; }
 
-       this.actions.push({
-         ticker: this.stocks[i].ticker,
-         action: delta
-       });
+        var optimal = this.optimal_calc(this.stocks[i].price, this.capital, this.stocks[i].target);
+        var delta = this.rebalance(optimal, this.stocks[i].quantity);
+
+        // conditional for extra capital
+
+        //Calculate new quantity of each asset
+        this.stocks[i].quantity_new = this.stocks[i].quantity + delta
+
+        this.actions.push({
+          ticker: this.stocks[i].ticker,
+          action: delta
+        });
+
+      }
+
+      //Calculate the new market value of the portfolio
+      this.market_val_new = this.stocks.reduce(function(acc,val){
+        prod = val.price * val.quantity_new
+        return acc + prod
+      },0)
+
+      //If current market value is above initial capital due to rounding
+      if (this.market_val_new > this.capital) {
+        //Sort by price
+        sortByKey(this.stocks, this.stocks.price);
+
+          for (var i in this.stocks) {
+
+            if (this.market_val_new > this.capital) {
+              //Check if stock has buy signal from rebalancer
+              if (this.stocks[i].quantity_new > this.stocks[i].quantity) {
+                //If so reduce that stock's quantity by one and recalculate market value
+                this.stocks[i].quantity_new = this.stocks[i].quantity_new - 1
+                this.market_val_new = this.market_val_new - this.stocks[i].price
+
+              }
+            } else {break;}
+          }
+
+          //Determine new cash balance and clear actions
+          this.cash = this.capital - this.market_val_new
+          this.actions = [];
+
+          //Recreate actions for console log
+          for (var i in this.stocks) {
+            this.actions.push({
+              ticker: this.stocks[i].ticker,
+              action: this.stocks[i].quantity_new - this.stocks[i].quantity
+            });
+
+          }
+        }
+
+     //Post to console if the sum of target weights is above 1.
+     if (this.target_check > 1) {
+
+       console.log("Your target weights sum to over 1. Please check and try again.");
+
+     } else {
+
+       console.log(this.capital);
+       console.log(this.actions);
+       console.log(this.market_val_new);
+       console.log(this.cash);
      }
-     console.log(this.capital);
-     console.log(this.actions);
-     console.log(this.market_val);
    };
 
  }
