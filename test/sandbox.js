c6feb450eba97bd70721deb112f9bcd73854052c
Chanon Roy <chanonroy@gmail.com>
2017-08-02 19:36:37 -0400

    New additions to script (#2)
    * transaction costs for 2 assets
    
    * 2 asset with transaction costs

--- test/sandbox.js
+++ test/sandbox.js
@@ -20,8 +20,15 @@ var portfolio = [
     value: 36.8
   }
 ]
+//Enter variable cost here
+var v_cost_buy = 5;
+var v_cost_sell = 0;
+
+//Enter fixed cost here
+var f_cost_buy = 0;
+var f_cost_sell = 0;
 
 // Instantiate object, compute, and log to console
-var rb = new Rebalancer(portfolio, cash);
+var rb = new Rebalancer(portfolio, cash, v_cost_buy, v_cost_sell, f_cost_buy, f_cost_sell);
 var result = rb.main();
 console.log(JSON.stringify(result));
